3e4471b53297ee15fab5f2dc596db4908539db55
Julian <ja.malaverm1@uniandes.edu.co>
2026-09-21 20:50:27 -0500

    En infra:le dimos mas recursos a  Fargate para manejar LoRA inference

--- infra/intencite/alb.tf
+++ infra/intencite/alb.tf
@@ -4,6 +4,8 @@ resource "aws_lb" "intencite" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb.id]
   subnets            = data.aws_subnets.default.ids
+
+  idle_timeout = 120
 }
 
 resource "aws_lb_target_group" "app" {
